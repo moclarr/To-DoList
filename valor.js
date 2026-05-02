8029bc4fa0467ea9a9d5538701346b505ccf2629
Clara Moreira <clarxmoreira18@gmail.com>
2026-05-02 19:39:04 -0300

    Delete valor.js

--- valor.js
+++
@@ -1,6 +0,0 @@
-function converter(){
-    document.querySelector("h1").style.color = "red"
-    let valorReal = document.querySelector("input").value;
-    let valorDolar = valorReal / 5
-    alert(`Valor convertido em Dólar: $ ${valorDolar}`)}
-
